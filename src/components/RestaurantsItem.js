bf8959e33060187490a40d686ee6e3ed2849f599
Augustine Mwangi Kariuki <augustinemwangi04@gmail.com>
2023-05-02 19:54:07 +0300

    finished with restaurantlist file, remaining with styling only

--- src/components/RestaurantsItem.js
+++ src/components/RestaurantsItem.js
@@ -1,20 +1,14 @@
-import React from 'react';
+// import React from 'react';
 
-const RestaurantItem = () => {
-  return (
-    <div className="restaurant-item">
-      <img src={restaurant.image} alt={restaurant.name} />
-      <h2>{restaurant.name}</h2>
-      <p>{restaurant.location}</p>
-      <button>Order Now</button>
-    </div>
-  );
-};
-
-e
-
-
-
-
+// const RestaurantItem = () => {
+//   return (
+//     <div className="restaurant-item">
+//       <img src={restaurant.image} alt={restaurant.name} />
+//       <h2>{restaurant.name}</h2>
+//       <p>{restaurant.location}</p>
+//       <button>Order Now</button>
+//     </div>
+//   );
+// };
 
 // export default RestaurantItem;

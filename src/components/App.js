d4af1692163b63dd8fdcf0e1080a1b8e6e6f86e1
TammyBriggs <tamunotonye.briggs@student.moringaschool.com>
2023-05-02 20:57:30 +0300

    App.css moved into Component folder

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Accommodation from "./AccommodationsList"
 import DestinationsItem from "./DestinationsItem";
 import DestinationsList from "./DestinationsList";
+import './App.css';
 import NavBar from "./NavBar";
 import RestaurantsList from "./RestaurantsList";
 

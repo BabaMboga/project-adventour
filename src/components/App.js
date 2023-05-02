53497578b0f768537a87a93d1affcbcf1782caf4
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-05-02 19:40:13 +0300

    update on App.js.

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,29 @@
-import React from "react";
+// import React from "react";
+// import Accommodation from "./AccommodationsList"
+// import DestinationsItem from "./DestinationsItem";
+// import DestinationsList from "./DestinationsList";
+// import NavBar from "./NavBar";
+// import RestaurantsList from "./RestaurantsList";
 
 
 
-function App() {
-  return (
-    <div>     
-     <h1>React</h1>
-    </div>
-  );
-}
 
-export default App;
+// function App() {
+//   return (
+//     <div>     
+//      <h1>My react Project</h1>
+//      {/* <NavBar /> */}
+//      {/* <NavBar.css /> */}
+//      <Accommodation />
+//      {/* <About /> */}
+//      <DestinationsItem />
+//      {/* <DestinationsList /> */}
+//      <RestaurantsList />
+
+//     </div>
+//   );
+// }
+
+// export default App;
 
 

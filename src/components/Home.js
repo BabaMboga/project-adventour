0b8a7af1e92e2f8629eaf1cde5d4f626e2d4e47a
TammyBriggs <tamunotonye.briggs@student.moringaschool.com>
2023-05-02 23:53:59 +0300

    NavBar styled

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <section className='home'>
+        <div className='secContainer container'>
+            <div className='homeText'>
+                <h1 className='title'>
+                    Plan Your Next Adventure with Adventour
+                </h1>
+                <p className='subTitle'>
+                    Explore Nairobi's finest destinations and immerse yourself in it's rich culture.
+                </p>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Home

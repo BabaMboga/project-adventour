9683788b9894ee327edc6061bf6a2f25ff84cd1d
Augustine Mwangi Kariuki <augustinemwangi04@gmail.com>
2023-05-02 07:35:30 +0300

    listed down the names of the hotels in Nairobi

--- src/components/RestaurantsList.js
+++ src/components/RestaurantsList.js
@@ -3,11 +3,60 @@ import React from 'react'
 
 function RestaurantsList() {
   return (
-    <div>
+    <div className='container'>
         <h1>THE RESTAURANTS IN NAIROBI</h1>
         <div className='cards'>
-            
+            <img src="https://static.wixstatic.com/media/4b855c29f0fa4015b5e1adfda7e53fde.jpg/v1/fill/w_636,h_590,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/4b855c29f0fa4015b5e1adfda7e53fde.jpg" alt="" />
+            <h2> Restaurants around town</h2>
+        </div>
 
+        <div className='text'>
+            <p3>
+                Crowne Plaza Nairobi Airport, an IHG Hotel
+                Airport
+                <br />
+                Ibis Styles
+                Westlands
+                <br />
+                The Marvel House
+                National Museum
+                <br />
+                Natural Oak ApartmentsOpens in new window
+                Kilimani
+                <br />
+                After 40 Hotel
+                CBD
+                <br />
+                Trademark Hotel
+                Gigiri 
+                <br />
+                The Panari Hotel
+                Mombasa Road 
+                <br />
+                Kandiz Exquisite
+                Lavington 
+                <br />
+                Villa Rosa Kempinski
+                Westlands 
+                <br />
+                Melili HotelOpens in new window
+                Westlands 
+                <br />
+                Hilton Garden
+                Airport 
+                <br />
+                Kahama Hotel
+                City Center 
+                <br />
+                Jatiq Homes
+                KICC 
+                <br />
+                Lenana Suites
+                Lenana 
+                <br />
+                Deuts Four Seasons HotelOpens in new window
+                Parklands 
+            </p3>
         </div>
     </div>
   )

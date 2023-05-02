b9418378b90fa94ff9077c7f49dcaf6f5addebdf
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-05-02 15:09:30 +0300

    Accomodation list final update

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AccommodationsList from "./AccomodationsList";
+
+const App = () => {
+  return (
+    <div>     
+      <AccommodationsList />
+    </div>
+  );
+}
+
+export default App;
+
+

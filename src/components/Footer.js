7eb2ad2c8b58bb24bb83282264f7e574e029844d
Augustine Mwangi Kariuki <augustinemwangi04@gmail.com>
2023-05-03 11:44:31 +0300

    updated the footer

---
+++ src/components/Footer.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="footer-col">
+                    <h4>company</h4>
+                    <ul1>
+                        <li><a href="#Home">Home</a></li>
+                        <li><a href="#Event Destinations">Event Destinations</a></li>
+                        <li><a href="#Where To Eat">Where To Eat</a></li>
+                        <li><a href="#Where To Stay">Where To Stay</a></li>
+                    </ul1>
+                </div>
+                <div class="footer-col">
+                    <h4>get help</h4>
+                    <ul1>
+                        <li><a href="#"></a></li>
+                        <li><a href="#">shipping</a></li>
+                        <li><a href="#">returns</a></li>
+                        <li><a href="#">order status</a></li>
+                        <li><a href="#">payment options</a></li>
+                    </ul1>
+                </div>
+                
+                <div class="footer-col">
+                    <h4>follow us</h4>
+                    <ul1>
+                    <div class="social-links">
+                        <a href="https://www.facebook.com/search/top?q=tk%20adventures"><i class="fab fa-facebook-f"></i></a>
+                        <a href="https://twitter.com/TKAdventure"><i class="fab fa-twitter"></i></a>
+                        <a href="https://www.instagram.com/tkadventures254/"><i class="fab fa-instagram"></i></a>
+                        <a href="https://www.myglobalviewpoint.com/adventure-quotes/"><i class="fab fa-whatsapp"></i></a>
+                    </div>
+                    </ul1>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer

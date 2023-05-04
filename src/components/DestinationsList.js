6c0ab875a6390ffa1a277311157f047efdbbe209
Augustine Mwangi Kariuki <augustinemwangi04@gmail.com>
2023-05-04 20:34:32 +0300

    styling the restaurant page

--- src/components/DestinationsList.js
+++ src/components/DestinationsList.js
@@ -7,7 +7,7 @@ function DestinationsList() {
   const [searchWord, setSearchWord] = useState("");
 
   useEffect(() => {
-    fetch("http://localhost:3005/destinations")
+    fetch("http://localhost:4000/destinations")
       .then((response) => response.json())
       .then((destinations) => setDestinations(destinations));
   }, []);

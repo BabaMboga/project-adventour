054fde82710273ff37192dec21407d40fa41d4b5
AyimWilliam <odhisayim@gmail.com>
2023-05-04 14:31:44 +0300

    Client-side routing 1st attempt

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 // import ScrollToTop from "./ScrollToTop";
 import NavBar from "./NavBar";
@@ -6,29 +7,31 @@ import About from "./About";
 import Services from "./Services";
 import Recommend from "./Recommend";
 import Testimonials from "./Testimonials";
-import AccommodationsList from "./AccommodationsList"
+import AccommodationsList from "./AccommodationsList";
 import DestinationsList from "./DestinationsList";
 import RestaurantsList from "./RestaurantsList";
 import Footer from "./Footer";
 
-
-
 function App() {
-
   return (
-    <div>     
-      {/* <ScrollToTop /> */}
-      <NavBar />
-      <About />
-      <Services />
-      <Recommend />
-      <Testimonials />
-      <AccommodationsList />
-      <RestaurantsList />
-      <DestinationsList/>
-      <Footer />
-    </div>
-  )
+    <Router>
+      <div>
+        {/* <ScrollToTop /> */}
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={About} />
+          <Route path="/accommodations" component={AccommodationsList} />
+          <Route path="/destinations" component={DestinationsList} />
+          <Route path="/restaurants" component={RestaurantsList} />
+        </Switch>
+        <About />
+        <Services />
+        <Recommend />
+        <Testimonials />
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;

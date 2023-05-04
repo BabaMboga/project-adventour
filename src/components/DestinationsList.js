8a45f4f932a3cb854d69b4482d4811e2cd6e7563
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-05-04 20:44:11 +0300

    pull work

--- src/components/DestinationsList.js
+++ src/components/DestinationsList.js
@@ -20,7 +20,7 @@ function DestinationsList() {
     const destinationToUpdate = destinations.find((destination) => destination.id === id);
     const updatedDestination = {...destinationToUpdate, likes: destinationToUpdate.likes + 1};
 
-    fetch(`http://localhost:4000/${id}`, {
+    fetch(`http://localhost:3005/destinations/${id}`, {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json',

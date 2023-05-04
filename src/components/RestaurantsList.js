b629492bb4b834f1f5c9f6bec2ecdd2a8a8f5d39
Augustine Mwangi Kariuki <augustinemwangi04@gmail.com>
2023-05-04 13:47:29 +0300

    styled the restaurantslist

--- src/components/RestaurantsList.js
+++ src/components/RestaurantsList.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from "react";
 import SearchBar from "./SearchBar";
 import RestaurantsItem from "./RestaurantsItem";
+import "./RestaurantsList.css"
 
 function RestaurantsList() {
   const [restaurants, setRestaurants] = useState([]);

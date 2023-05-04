5953d3b750982bb00caa100156b4d3c22cc61ce9
Augustine Mwangi Kariuki <augustinemwangi04@gmail.com>
2023-05-04 15:30:51 +0300

    solving errors from the browser

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ import AccommodationsList from "./AccommodationsList";
 import DestinationsList from "./DestinationsList";
 import RestaurantsList from "./RestaurantsList";
 import Footer from "./Footer";
-import Login from "./Login";
+// import Login from "./Login";
 
 function App() {
   return (
